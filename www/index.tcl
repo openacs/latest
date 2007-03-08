0b56bb6ba3256ef1347e5b875cbc1278a4af5537
Cesar Claveria <cesar@viaro.net>
2007-03-08 16:00:19 +0000

    adding index.tcl

---
+++ www/index.tcl
@@ -0,0 +1,13 @@
+# /lastest/www/index.tcl
+ad_page_contract {
+    Left Menu
+    @author Hector Amado (hr_amado@viaro.net)
+    @creation-date 24-03-2006
+
+} {
+{ itemId "" }
+{ object "" }
+{ newURL ""}
+}
+
+#do nothing
